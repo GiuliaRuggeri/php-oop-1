31e345f6ffaed369cea5ffe5cd5f0431c7c1b2be
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-01-12 15:56:25 +0100

    Bonus 2 & 3

--- Models/Movie.php
+++ Models/Movie.php
@@ -7,18 +7,37 @@ class Movie
     public $year;
     public $genre;
     public $language;
+    public $actor;
+    public $director;
+    public $country;
+    public $runningtime;
 
-    public function __construct($name, $year, $genre, $language) {
+    public function __construct($name, $year, $genre, $language, $actor, $director, $country, $runningtime) {
 
         $this->name=$name;
         $this->year=$year;
         $this->genre=$genre;
         $this->language=$language;
+        $this->actor=$actor;
+        $this->director=$director;
+        $this->country=$country;
+        $this->runningtime=$runningtime;
         
     }
 
     public function getMovieInfo(){
 
-        $info=$this->name .",". $this->year .",". $this->genre .",". $this->language;
+        $info=$this->name .",". $this->year .",". implode(',', $this->genre) .",". $this->language .",". implode(',', $this->actor) .",". $this->director .",". $this->country .",". $this->runningtime;
+        return $info; 
+    }
+
+    public function getGenreList(){
+        $genres=implode(',', $this->genre);
+        return $genres;
+    }
+
+    public function getActor(){
+        $actors=implode(',', $this->actor);
+        return $actors;
     }
 }
--- dati.php
+++ dati.php
@@ -2,15 +2,60 @@
 
 require __DIR__ . "/Models/Movie.php";
 
-$BackToTheFuture= new Movie("Back to the future", 1985, "fantasy", "english");
-$TheLostBoys= new Movie("The lost boys", 1987, "horror", "english");
+$genres1 = [
+    "fantasy", "comedy"
+];
 
-$movies=[
+$genres2 = [
+    "horror", "comedy"
+];
+
+$actors1 = [
+
+    "Michael J. Fox", "Christopher Lloyd", "Claudia Wells"
+];
+
+$actors2 = [
+
+    "Kiefer Sutherland", "Corey Haim", "Corey Feldman"
+
+];
+
+$BackToTheFuture = new Movie("Back to the future", 1985, $genres1, "english", $actors1, "Robert Zemeckis", "USA", 116);
+$TheLostBoys = new Movie("The lost boys", 1987, $genres2, "english", $actors2, "Joel Schumacher", "USA", 98);
+
+$movies = [
     $BackToTheFuture,
     $TheLostBoys,
 ];
-
-
-var_dump($movies);
-
 ?>
+
+<table class="table text-center">
+    <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Year</th>
+            <th scope="col">Genres</th> 
+            <th scope="col">Actors</th>
+            <th scope="col">Director</th>
+            <th scope="col">Country</th>
+            <th scope="col">Running time</th>
+            <th scope="col">Language</th>
+           
+        </tr>
+    </thead>
+    <tbody>
+
+        <?php
+        foreach ($movies as $movie) {
+            echo  "<tr><td>" . $movie->name . "</td>";
+            echo  "<td>" . $movie->year . "</td>";
+            echo  "<td>" . $movie->getGenreList() . "</td>";
+            echo  "<td>" . $movie->getActor() . "</td>";
+            echo  "<td>" . $movie->director . "</td>";
+            echo  "<td>" . $movie->country . "</td>";
+            echo  "<td>" . $movie->runningtime . "</td>";
+            echo  "<td>" . $movie->language . "</td></tr>";
+        };
+
+        ?>
